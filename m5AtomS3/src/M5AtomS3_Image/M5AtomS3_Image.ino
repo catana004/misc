1b3f583743b94fb301c6e4bf991ad5eda32becb7
catana004 <t_matsuura@catana.co.jp>
2023-08-01 22:23:33 +0900

    AtomS3 の画面にグラフィック出せたぞ

---
+++ m5AtomS3/src/M5AtomS3_Image/M5AtomS3_Image.ino
@@ -0,0 +1,169 @@
+#include <M5AtomS3.h>
+
+const int monCh1 = 5 ;
+const int monCh2 = 6 ;
+const int monCh3 = 7 ;
+const int monCh4 = 8 ;
+const int genCh1 = 38 ;
+const int genCh2 = 39 ;
+
+const int SAMPLEMAX = 256 ;   // 128dot * 2
+struct TSAMPLES {
+  unsigned int datas[SAMPLEMAX] ;
+  int trigIdx = -1 ;
+  int afterTrig = 0 ;
+  int smpIdx = 0 ;
+  int displayed = 0 ;
+} ;
+
+TSAMPLES samples[2] ;
+int samplePane = 0 ;
+
+int sampleCount = 0 ;
+void taskSampling(void* pvParameters) {  // Define the tasks to be executed in
+    
+  // thread 1.  定义线程1内要执行的任务
+  TSAMPLES * psamp ;
+  TSAMPLES * psampback ;
+
+  int data = 0 ;
+  while (1) {  // Keep the thread running.  使线程一直运行
+    psamp = &samples[samplePane] ;
+    data = 0 ;
+    if ( digitalRead(monCh1) == HIGH )
+      data = 1 ;
+    if ( digitalRead(monCh2) == HIGH )
+      data += 2 ;
+    if ( digitalRead(monCh3) == HIGH )
+      data += 4 ;
+    if ( digitalRead(monCh4) == HIGH )
+      data += 8 ;
+    psamp->datas[psamp->smpIdx] = data ;
+    psamp->smpIdx ++ ;
+    if ( psamp->smpIdx >= SAMPLEMAX )
+      psamp->smpIdx = 0 ;
+    
+//  トリガ成立のダミー -->>
+if (psamp->smpIdx == 150 )
+  psamp->trigIdx = psamp->smpIdx ;
+//  トリガ成立のダミー --<<
+
+    if ( psamp->trigIdx >= 0 ){
+      psamp->afterTrig ++ ;
+      if ( psamp->afterTrig > SAMPLEMAX / 2 ){
+        //  サンプル満タン完了
+        //  裏に移動（表示完了していたら、ってこともあるかも）
+        int nextPane ;
+        if ( samplePane == 0 )
+          nextPane = 1 ;
+        else
+          nextPane = 0 ;
+        //  裏を初期化
+        psampback = &samples[nextPane] ;
+        if ( psampback->displayed != 0 ){
+          //  裏が表示済みなら裏確定
+          samplePane = nextPane ;
+        }else{
+          //  今のを上書きする
+          psampback = psamp ;
+        }
+        psampback->trigIdx = -1 ;
+        psampback->afterTrig = 0 ;
+        psampback->smpIdx = 0 ;
+        psampback->displayed = 0 ;
+        sampleCount ++ ;
+        USBSerial.print("sample ");
+        USBSerial.println(sampleCount) ;
+      }
+    }else
+      psamp->afterTrig = 0 ;
+
+
+
+//      USBSerial.print("taskSampling Uptime (ms): ");
+//      USBSerial.println(
+//          millis());  // The running time of the serial port
+//                      // printing program.  串口打印程序运行的时间
+//      delay(
+//          100);  // With a delay of 100ms, it can be seen in the serial
+                  // monitor that every 100ms, thread 1 will be executed once.
+                  // 延迟100ms,在串口监视器内可看到每隔100ms,线程1就会被执行一次
+  }
+}
+
+void taskDraw(void* pvParameters) {  // Define the tasks to be executed in
+                                  // thread 1.  定义线程1内要执行的任务
+  TSAMPLES * psamp ;
+  TSAMPLES * psampback ;
+
+
+  while (1) {  // Keep the thread running.  使线程一直运行
+    if ( samplePane == 0 )
+      psamp = &samples[1] ;
+    else
+      psamp = &samples[0] ;
+
+    if ( psamp->displayed != 0 ){
+      //  表示済みなら少し待つ
+      delay(100) ;
+    }else{
+      //  表示前なら、新しいデータ。表示する
+
+
+
+
+      //  表示済みフラグを立てる
+      psamp->displayed = 1 ;
+
+    }
+      USBSerial.print("taskDraw Uptime (ms): ");
+      USBSerial.println(
+          millis());  // The running time of the serial port
+                      // printing program.  串口打印程序运行的时间
+      delay(
+          100);  // With a delay of 100ms, it can be seen in the serial
+                  // monitor that every 100ms, thread 1 will be executed once.
+                  // 延迟100ms,在串口监视器内可看到每隔100ms,线程1就会被执行一次
+  }
+}
+
+
+void setup()
+{
+  M5.begin(true,true,false,false) ;
+
+  M5.Lcd.fillRect(10,10,30,30,RED) ;
+  M5.Lcd.println( "TEST") ;
+  M5.Lcd.println( "TEST1") ;
+  M5.Lcd.println( "TEST2") ;
+
+  pinMode(monCh1,INPUT) ;
+  pinMode(monCh2,INPUT) ;
+  pinMode(monCh3,INPUT) ;
+  pinMode(monCh4,INPUT) ;
+
+  pinMode(genCh1,OUTPUT) ;
+  pinMode(genCh2,OUTPUT) ;
+
+    // Creat Task1.  创建线程1
+    xTaskCreatePinnedToCore(
+        taskSampling,    // Function to implement the task.
+                  // 线程对应函数名称(不能有返回值)
+        "taskSample",  //线程名称
+        4096,     // The size of the task stack specified as the number of *
+                  // bytes.任务堆栈的大小(字节)
+        NULL,     // Pointer that will be used as the parameter for the task *
+                  // being created.  创建作为任务输入参数的指针
+        1,        // Priority of the task.  任务的优先级
+        NULL,     // Task handler.  任务句柄
+        1);  // Core where the task should run.  将任务挂载到指定内核
+
+    // Task 2
+    xTaskCreatePinnedToCore(taskDraw, "taskDraw", 4096, NULL, 2, NULL, 0);
+
+}
+
+void loop()
+{
+  //  GET BIT FROM PIO
+}

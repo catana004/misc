f4b604410e8ad88d35b2a32626d0c8ce04439acc
catana004 <t_matsuura@catana.co.jp>
2023-08-04 19:58:35 +0900

    Win32Api GetDiskFreeSpaceEx でネットドライブの空きが取れたよ。よかった。

--- WinDirFree/WinDirFree/Form1.cs
+++ WinDirFree/WinDirFree/Form1.cs
@@ -9,14 +9,24 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using System.Management;
+using System.Runtime.InteropServices;
 
-[System.ComponentModel.TypeConverter(typeof(System.Management.ManagementScopeConverter))]
-public class ManagementScope : ICloneable
+//[System.ComponentModel.TypeConverter(typeof(System.Management.ManagementScopeConverter))]
+//public class ManagementScope : ICloneable
+
 
 namespace WinDirFree
 {
     public partial class Form1 : Form
     {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool GetDiskFreeSpaceEx(
+        string lpDirectoryName,
+        out ulong lpFreeBytesAvailable,
+        out ulong lpTotalNumberOfBytes,
+        out ulong lpTotalNumberOfFreeBytes);
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +34,17 @@ namespace WinDirFree
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            {
+                String netpath = "\\\\cat-nas1\\作業用_松浦";
+                ulong freeBytesAvailable;
+                ulong totalNumberOfBytes;
+                ulong totalNumberOfFreeBytes;
+
+                GetDiskFreeSpaceEx(netpath, out freeBytesAvailable, out totalNumberOfBytes, out totalNumberOfFreeBytes);
+
+                Console.WriteLine("FreeBytesAvailable {0} totalBytes {1} totalFreeBytes {2}",
+                    freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes);
+            }
             {
                 /*// Build an options object for the remote connection
                 //   if you plan to connect to the remote
@@ -49,7 +70,8 @@ namespace WinDirFree
                 // remote computer.
                 ManagementScope scope =
                     new ManagementScope(
-                    "\\\\FullComputerName\\root\\cimv2");
+                        @"\\cat-nas1");
+//                @"\\cat-nas1\作業用_松浦");
                 scope.Connect();
 
                 // Use this code if you are connecting with a
@@ -62,23 +84,37 @@ namespace WinDirFree
 
                 //Query system for Operating System information
                 ObjectQuery query = new ObjectQuery(
-                    "SELECT * FROM Win32_OperatingSystem");
+                "SELECT * FROM Win32_LogicalDisk"
+                //"SELECT * FROM Win32_Directory"
+                );
                 ManagementObjectSearcher searcher =
                     new ManagementObjectSearcher(scope, query);
 
                 ManagementObjectCollection queryCollection = searcher.Get();
                 foreach (ManagementObject m in queryCollection)
                 {
-                    // Display the remote computer information
-                    Console.WriteLine("Computer Name : {0}",
-                        m["csname"]);
-                    Console.WriteLine("Windows Directory : {0}",
-                        m["WindowsDirectory"]);
-                    Console.WriteLine("Operating System: {0}",
-                        m["Caption"]);
-                    Console.WriteLine("Version: {0}", m["Version"]);
-                    Console.WriteLine("Manufacturer : {0}",
-                        m["Manufacturer"]);
+                    var v1 = m.Properties;
+                    Console.WriteLine("=============");
+                    foreach( var v2 in v1)
+                    {
+                        Console.WriteLine("Key {0} Value {1}" , v2.Name , v2.Value);
+
+                    }
+                    //foreach( var n in m)
+                    //{
+                    //    Console.WriteLine("Key {0} Value {1}", n.Key , n.Value );
+                    //}
+
+                    //// Display the remote computer information
+                    //Console.WriteLine("Computer Name : {0}",
+                    //    m["csname"]);
+                    //Console.WriteLine("Windows Directory : {0}",
+                    //    m["WindowsDirectory"]);
+                    //Console.WriteLine("Operating System: {0}",
+                    //    m["Caption"]);
+                    //Console.WriteLine("Version: {0}", m["Version"]);
+                    //Console.WriteLine("Manufacturer : {0}",
+                    //    m["Manufacturer"]);
                 }
             }
         }
